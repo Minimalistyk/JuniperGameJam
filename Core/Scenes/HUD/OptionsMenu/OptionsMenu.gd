e477f9320804929e864d0e1cda75f73a68744f19
Minimalistyk <michalminimalistykmakos0405@gmail.com>
2026-06-17 19:25:36 +0200

    Dodano menu ustawien oraz logike wyświetlania go z menu start/pauzy

---
+++ Core/Scenes/HUD/OptionsMenu/OptionsMenu.gd
@@ -0,0 +1,32 @@
+class_name OptionsMenu
+extends Control
+@onready var options_menu_container: PanelContainer = %OptionsMenuContainer
+var target_x: float
+
+func _ready() -> void:
+	SignalManager._on_options_menu_show.connect(on_options_menu_show)
+	target_x = options_menu_container.position.x
+	options_menu_container.position.x = 1280
+	visible = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("Escape"):
+		if visible:
+			get_viewport().set_input_as_handled()
+			back()
+
+func on_options_menu_show() -> void:
+	visible = true
+	var tween: Tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUART)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(options_menu_container, "position:x", target_x, 0.3).from(1280.0)
+
+func back() -> void:
+	var tween: Tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUART)
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(options_menu_container, "position:x", 1280.0, 0.2)
+	tween.tween_callback(func(): visible = false)
+	SignalManager.emit_on_options_menu_close()
+	
--- Core/Scenes/HUD/PauseMenu/PauseMenu.gd
+++ Core/Scenes/HUD/PauseMenu/PauseMenu.gd
@@ -1,46 +1,59 @@
+class_name PauseMenu
 extends Control
 @onready var anim_player_pause_menu: AnimationPlayer = %AnimPlayerPauseMenu
 @onready var pause_menu_container: PanelContainer = %PauseMenuContainer
 @onready var black_rect: ColorRect = %BlackRect
 
 var target_y: float
+var options_up: bool
 
 func _ready() -> void:
+	SignalManager._on_options_menu_close.connect(on_options_menu_close)
 	target_y = pause_menu_container.position.y
 	resume()
+	
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Escape"):
+		if options_up:
+			return
 		if get_tree().paused:
 			resume()
 		else:
 			show_menu()
 
-func show_menu() -> void:
+func show_anim() -> void:
 	pause_menu_container.visible = true
-	PauseManager.set_pause(true)
-	anim_player_pause_menu.play("blur")
-	
 	var tween: Tween = create_tween()
 	tween.set_trans(Tween.TRANS_QUART)
 	tween.set_ease(Tween.EASE_OUT)
-	tween.tween_property(pause_menu_container, "position:y", target_y, 0.4).from(720.0)
+	tween.tween_property(pause_menu_container, "position:y", target_y, 0.3).from(720.0)
+
+func show_menu() -> void:
+	PauseManager.set_pause(true)
+	anim_player_pause_menu.play("blur")
+	show_anim()
+
+func resume_anim() -> void:
+	var tween: Tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUART)
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(pause_menu_container, "position:y", 720.0, 0.2)
+	tween.tween_callback(func(): pause_menu_container.visible = false)
 
 func resume() -> void:
 	PauseManager.set_pause(false)
 	anim_player_pause_menu.play_backwards("blur")
-	
-	var tween: Tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUART)
-	tween.set_ease(Tween.EASE_IN)
-	tween.tween_property(pause_menu_container, "position:y", 720.0, 0.3)
-	tween.tween_callback(func(): pause_menu_container.visible = false)
+	resume_anim()
 
 func open_options() -> void:
-	print(PauseManager.current_pause)
-	pass	#powodzenia XD, przy okazji skasuj tą debudowa linijke
-			#i prosze użyj tweena aby ustawienia wyskoczyly z dołu ekranu a menu pauzy sie schowalo w dół
-			#mysle ze te tweeny wystaczy wtedy jebanac do osoncyh funkcji z parametrami co tweenujemy, skąd i dokąd :D
+	options_up = true
+	SignalManager.emit_on_options_menu_show()
+	resume_anim()
+
+func on_options_menu_close() -> void:
+	options_up = false
+	show_anim()
 
 func quit() -> void:
 	black_rect.visible = true
